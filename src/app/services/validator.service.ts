b1157940465e0ea774ff9283a4610bde2b17e5a8
sonthen <sonsonthen@gmail.com>
2017-09-27 16:08:06 +0700

    dari daniel, edit kosmetik dan tambah service untuk parsing data ke backed di validator.service

--- src/app/services/validator.service.ts
+++ src/app/services/validator.service.ts
@@ -1,23 +1,41 @@
 import { Injectable } from '@angular/core';
 
+import { Http, Headers, RequestOptions} from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch'; 
+
 @Injectable()
 export class ValidatorService {
 
-  constructor() { }
+  constructor(private http:Http) { }
 
   noPonsel = '';
-  ponselLength: number;
   hpIsInvalid = false;
   hpTelahDikirim = false;
-  
   validateHP () {
     
-    const reg = /^\d+$/;
-    if (reg.test(this.noPonsel) && this.noPonsel.length > 9 && this.noPonsel.length < 13) {
+    const reg = /^08[0-9]{8,11}$/;
+    if (reg.test(this.noPonsel)) {
+      
+      let data = {
+        "phone_number": this.noPonsel,
+      };
+      let body = JSON.stringify(data);
+      let headers = new Headers ({ 
+        "Content-Type":"application/json"
+      });
+      
+      let options = new RequestOptions({ headers : headers});
+  
+      
       
       this.hpIsInvalid = false;
       this.hpTelahDikirim = true;
       this.noPonsel="";
+      return this.http.post('http://localhost:8000/api/addPonsel', body, options)
+      .map(result=> result.json());
 
     }
     else{
@@ -31,6 +49,7 @@ export class ValidatorService {
   email = '';
   nama = '';
   namaSalah = false;
+  namaBelumSalah = true;
   namaIsNull = true;
   emailDikirim = false;
   emailBelumBenar = true;
@@ -52,16 +71,33 @@ export class ValidatorService {
   }
   
   langgananEmail () {
-    const reg =  /^[a-zA-Z]*$/;
+    const reg = /^[a-zA-Z]([a-zA-Z ,.]*)$/;
+    // /^[a-zA-Z]*$/;
     if (reg.test(this.nama)) {
       
       console.log("langganan email !");
       this.emailDikirim=true;
       this.namaSalah=false;
       this.emailValid = false;
+
+      //kirimKeEmailDB () => dari validator.service
+      let data = {
+        "name": this.nama,
+        "email": this.email,
+      };
+      let body = JSON.stringify(data);
+      let headers = new Headers ({ 
+        "Content-Type":"application/json"
+      });
+      
+      let options = new RequestOptions({ headers : headers});
+  
+      return this.http.post('http://localhost:8000/api/addEmail', body, options)
+      .map(result=> result.json());
     }
     else{
       this.namaSalah=true;
+      this.namaBelumSalah=false;
     }
     
   }
